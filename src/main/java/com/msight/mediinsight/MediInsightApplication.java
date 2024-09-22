26874a53d238d9ef9980bf1462325836e3600777
KrowFeather <2625056626@qq.com>
2024-09-22 23:43:31 +0800

    init

---
+++ src/main/java/com/msight/mediinsight/MediInsightApplication.java
@@ -0,0 +1,13 @@
+package com.msight.mediinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediInsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediInsightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msight/mediinsight/MediInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msight.mediinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediInsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
